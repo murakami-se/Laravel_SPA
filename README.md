e35fddf0fe8a391cbb814b86bacc240618d15117
r-murakami <risa.murakami.work@gmail.com>
2021-07-31 19:51:39 +0900

    [chore] browserSync導入

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Laravel_SPA
-Laravel8, React(mix)によるSPA開発環境
+Laravel8のSPA開発環境
+
+## versions
+
+- Laravel: 8.52.0
+- Node.js: 12.19.0
+- PHP: 7.4.1
+
+## branchs
+
+- main: laravel/ui, browserSync導入のみ
+    
+## browserSync使用方法
+
+1. `php artisan serve` で内部サーバー起動
+1. `npm run watch` でファイル監視
+1. http://localhost:3000 にアクセス
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,11 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+mix.browserSync({                // 変更を監視して自動でブラウザ更新
+        proxy: '127.0.0.1:8000', // ローカルサーバーアドレス
+        open: false              // ブラウザを自動で開かない
+    })
+    .js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         //
     ]);
